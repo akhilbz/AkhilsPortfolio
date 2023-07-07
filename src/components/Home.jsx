3df5ca1fe78ec4698255d776de01db36c5d39335
akhilbz <akhilesh.bitla@gmail.com>
2023-07-07 01:07:54 -0500

    Enhanced the front main page; still need to build and improve design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,17 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 const Home = () => {
     return (
         <div className="w-full h-screen bg-[#0a192f]">
+            <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+                <div className="flex ">
+                <p className="text-[#a18fd8] text-2xl sm:text-3xl font-light ">Welcome!&nbsp;</p>
+                <p className="text-[#a18fd8] text-2xl sm:text-3xl font-light ">My name is </p>
+                </div>
+                <h1 className="text-5xl sm:text-7xl text-[#FFFFE6] font-bold title-name">Akhilesh Bitla </h1>
+                <div className="parallelogram-top my-2"><h2 className="text-3xl sm:text-7xl text-[#333333] font-bold title-name text-center max-w-[750px]">CS Student @ UT Austin</h2></div>
+                <div className="parallelogram-btm my-3 mx-[-15px]"><p className="py-2 px-4 text-xl text-[#FFFFE6] font-medium max-w-[750px]">I'm passionate about designing and developing software applications that have a tangible 
+                and meaningful <span className="highlight-impact font-[Anton] text-lg text-[#333333]">IMPACT</span> on people's everyday lives.</p></div>
 
+            </div>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { FaBars, FaTimes, FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+import { FaBars, FaTimes, FaGithub, FaLinkedin, FaInstagram, FaGit } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { BiFile } from 'react-icons/bi';
 import { HiOutlineMenuAlt4, HiOutlineMail } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
 import { MdDescription } from 'react-icons/md';
 
 const Navbar = () => {
@@ -11,33 +12,42 @@ const Navbar = () => {
     return (
         <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
             <div>
-                <h1>AKHILESH BITLA</h1>
+                <h1 className='font-[Megrim] font-thin text-2xl text-white cursor-pointer glow'>AKHILESH BITLA</h1>
             </div>
 
             {/* menu */}
-            <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About Me</li>
-                <li>Education</li>
-                <li>Experience</li>
-                <li>Projects</li>
+            <ul className='hidden md:flex font-thin'>
+                <li className='hover:font-medium transition-all: duration-300 '>Home</li>
+                <li className='hover:font-medium transition-all: duration-300 '>About Me</li>
+                <li className='hover:font-medium transition-all: duration-300 '>Education</li>
+                <li className='hover:font-medium transition-all: duration-300 '>Experience</li>
+                <li className='hover:font-medium transition-all: duration-300 '>Projects</li>
             </ul>
 
             {/** Hamburger */}
             <div onClick={handleClick} className='md:hidden z-10'>
-                {!nav ? <HiOutlineMenuAlt4 /> : <FaTimes />}
+                {!nav ? <HiOutlineMenuAlt4 /> : <AiOutlineClose />}
             </div>
-
+        {/* bg-[#0a192f] */}
             {/** Mobile Menu */}
-            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-                <li className='py-6 text-4xl'>Home</li>
-                <li className='py-6 text-4xl'>About Me</li>
-                <li className='py-6 text-4xl'>Education</li>
-                <li className='py-6 text-4xl'>Experience</li>
-                <li className='py-6 text-4xl'>Projects</li>
-            </ul>
+            <div className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] px-4 py-6 flex flex-col'}>
+            <h1 className='font-[Megrim] font-thin text-2xl text-white'>AKHILESH BITLA</h1>
+                <ul className='items-center justify-center flex flex-col'>
+                    <li className=' py-6 text-3xl justify-center'>Home</li>
+                    <li className=' py-6 text-3xl'>About Me</li>
+                    <li className=' py-6 text-3xl'>Education</li>
+                    <li className=' py-6 text-3xl'>Experience</li>
+                    <li className=' py-6 text-3xl'>Projects</li>
+                    <div className="flex w-[50%] justify-between py-5">
+                        <FaLinkedin size={30} className='cursor-pointer mobile-linkedin'/>
+                        <FaGithub size={30} className='cursor-pointer mobile-github'/>
+                        <BsFillPersonLinesFill size={30} className='cursor-pointer mobile-contact'/>
+                        <BiFile size={30} className='cursor-pointer mobile-resume'/>
+                    </div>
+                </ul>
+            </div>
             {/** Socials */}
-            <div className='hidden md:flex fixed flex-col top-[35%] left-0'>
+            <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
                 <ul>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                         <a href="/" className='w-[160px] h-[60px] flex justify-between items-center text-gray-300'>
