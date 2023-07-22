593fb6448b3deddf5ab75810a80b38f3d876a531
akhilbz <akhilesh.bitla@gmail.com>
2023-07-22 15:15:02 -0500

    Added a favicon and updated some formatting

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -38,9 +38,9 @@ const About = () => {
         <div name="about" className="w-full h-[fit-content] bg-[#F7E3B6]">
             <div className="flex flex-col justify-center items-center w-full h-[fit-content]">
                 <div className="w-full h-[fit-content] xl:grid xl:grid-cols-2  ">
-                    <div className="sm:text-left lg:pl-[60px] sm:mx-[40px] sm:h-[fit-content] items-center flex flex-col">
+                    <div className="sm:text-left lg:pl-[60px] xl:mx-[10px] mx-[40px] sm:h-[fit-content] items-center flex flex-col">
                         <p className="text-5xl sm:text-8xl font-bold title-name justify-center flex pb-2 text-[#333333]" style={{ textDecoration: 'underline'}}>My Story</p>
-                        <div className="py-2 text-[#FFFFE6] bg-[#333333] justify-center items-start w-[fit-content] h-[fit-content] rounded-2xl sm:mt-3 m-2">
+                        <div className="py-2 text-[#FFFFE6] bg-[#333333] justify-center items-start w-[fit-content] h-[fit-content] rounded-2xl sm:mt-3 m-5">
                             <p className=" flex justify-center px-5 font-bold pb-[2px]" style={{ textIndent: '50px', fontSize: '1.05rem'}}>Hello! My name is Akhilesh Bitla. Before I delve in, thank you for checking out my portfolio! 
                             I really appreciate you for spending time reading about me and my work! As you probably can see, I still have a long way to go, 
                             but I am a dedicated, passionate, and diligent student, pushing through my limits to establish a successful career.</p>
@@ -55,7 +55,7 @@ const About = () => {
                             and gaining a comprehensive grasp of the holistic development process.</p>
                         </div>
                     </div>
-                    <div className="sm:pt-[140px]">
+                    <div className="xl:pt-[105px]">
                     <div className="justify-center items-center flex flex-col sm:mx-[170px] relative rounded-2xl bg-center bg-cover h-[645px] ">
                     <div style={{backgroundImage: `url(${gallery[currIndex].url})`, boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.75)' }} className=" group w-[390px] h-[590px] items-center flex justify-between rounded-2xl bg-center bg-cover duration-500">
                     <div className="hidden group-hover:block justify-start text-3xl p-2 text-white cursor-pointer">
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -51,7 +51,7 @@ const Education = () => {
     }
     return (
         <>
-        <div className="w-full h-full sm:flex bg-[#F7E3B6] flex pt-2 lg:flex-row flex-col">
+        <div className="w-full h-full sm:flex bg-[#F7E3B6] flex pt-[75px] lg:flex-row flex-col">
             <div  className=" flex-col flex items-center justify-center h-[fit-content] lg:pl-[80px] w-full px-2 pb-2">
             <h1 className="text-6xl sm:text-6xl md:text-8xl font-bold title-name justify-center flex text-[#FFFFE6] pb-4" style={{ textDecoration: 'underline'}}>Education</h1>
                 <img src={utAustin} alt="" className='rounded-2xl justify-center mb-4 w-[700px] flex' style={{ boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.75)' }}/>
@@ -66,7 +66,7 @@ const Education = () => {
                     <h3 className='text-2xl sm:text-3xl md:text-3xl font-bold  md:justify-end justify-center flex text-[#FFFFE6] cns-edu-title '><div className='text-[#a18fd8] text-2xl sm:text-3xl bg-[#333333] md:text-4xl rounded-xl h-[fit-content] year pr-1 pl-1 mb-1' >Sophomore</div>&nbsp;&#8226; BS in Computer Science</h3>
                     <h3 className='text-2xl sm:text-3xl font-bold lg:justify-end justify-center flex text-[#333333] cns-edu-title '> College of Natural Sciences</h3>
                     </div>
-                    <div className="flex-col lg:my-[40px] my-5 rounded-2xl bg-[#FFFFE6] w-[fit-content] mx-5 py-2">
+                    <div className="flex-col lg:my-[40px] my-5 rounded-2xl bg-[#FFFFE6] w-[fit-content] mx-[100px] py-2">
                     <p className='flex justify-center px-3 font-bold pb-1' style={{ textIndent: '50px', fontSize: '1.05rem'}}>I’m a rising Sophomore pursuing a CS degree with an Entrepreneurship Minor at the University of Texas at Austin. 
                         I have completed Data Structures, Discrete Math and Computer Architecture and Organization in my coursework. In Fall 2023, 
                         I will be learning Operating Systems. Then Algorithms in the Spring. This will conclude my core requirements for my degree.</p>
@@ -78,7 +78,7 @@ const Education = () => {
                     </div>
             </div>
         </div>
-        <div className="w-full h-full flex flex-col justify-center items-center bg-[#F7E3B6]">
+        <div className="w-full h-full flex flex-col justify-center items-center bg-[#F7E3B6] pt-[75px]">
             <h2 className="text-4xl sm:text-5xl md:text-5xl font-bold justify-end flex title-name text-[#FFFFE6]">Entrepreneurship Minor</h2>
             <h3 className='text-xl sm:text-2xl md:text-3xl font-bold justify-end flex text-[#333333] cns-edu-title sm:pt-3 pt-2'>Harkey Institute for Entrepreneurial Studies</h3>
             {/* <div className="sm:w-full w-[fit-content] flex-col"> */}
