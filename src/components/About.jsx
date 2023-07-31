14e80330ee2cdde1b97c1ae27ff6646f6cc4d583
akhilbz <akhilesh.bitla@gmail.com>
2023-07-30 23:15:25 -0500

    updated education and about section with modified information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,13 +41,13 @@ const About = () => {
                     <div className="lg:pl-[90px] sm:h-[fit-content] items-center flex flex-col">
                         <p className="text-5xl sm:text-8xl font-bold title-name justify-center flex pb-2 text-white" style={{ textDecoration: 'underline'}}>My Story</p>
                         <div name="edu" className=" py-2 text-[#CCCCCC] sm:text-xl text-lg font-light w-[fit-content] h-[fit-content] sm:mt-[20px] mx-[65px]">
-                            <p className=" mb-[10px]" >Hi there! My name is <span className="font-semibold text-white text-xl sm:text-2xl">Akhilesh Bitla</span>, and I want to thank you for showing interest in me and my work. 
-                            I'm passionate about <span className="font-semibold text-white">Computer Science</span> and always striving to overcome challenges. 
-                            The journey has been exciting and stressful, but I embrace mistakes and believe in a <span className="font-semibold text-[#FFD700]">growth mindset</span>. 
-                            Computer Science offers endless possibilities, from algorithms to elegant design principles. 
-                            I'm particularly interested in <span className="font-semibold text-[#FF5050]">full-stack development</span>, where I can explore integrating backend and frontend components to create holistic solutions.</p>
-                            <p>After developing a strong experience as a <span className="font-semibold text-[#FF5050]">Full Stack developer</span>, my goal is to enter the realm of <span className="font-semibold text-[#00CC66]">AI</span> and <span className="font-semibold text-[#87CEFA]">Machine Learning</span>, and 
-                            develop scalable and efficient software tools that can make an <span className="font-semibold text-white text-xl sm:text-2xl">IMPACT</span> on people's lives.</p>
+                            <p className=" mb-[10px]" >Hi there! My name is <span className="font-semibold text-white text-xl sm:text-2xl">Akhilesh Bitla</span>, and I want to thank you for checking out my website and showing interest in my work.
+                            I am passionate about <span className="font-semibold text-white">Computer Science</span> and love to develop software applications that can have a tangible impact on people’s lives.
+                            My journey with CS has been exciting and stressful, but I embrace mistakes and believe in a <span className="font-semibold text-[#FFD700] text-xl sm:text-2xl">growth mindset</span>. 
+                            Along with my Computer Science education at UT Austin, I am improving my skills in <span className="font-semibold text-[#FF5050]">full-stack development</span> by exploring the integration of backend and frontend components to create holistic solutions.</p>
+                            <p className="mb-[10px]">After developing a strong experience as a <span className="font-semibold text-[#FF5050]">Full Stack developer</span>, my goal is to enter the realm of <span className="font-semibold text-[#00CC66]">AI</span> and <span className="font-semibold text-[#87CEFA]">Machine Learning</span> and
+                            develop scalable and efficient software tools that can be used to optimize people's daily life.</p>
+                            <p>I am looking for SWE internships to enhance my skills as a Full Stack Developer.</p>
                         </div>
                     </div>
                     <div className="xl:pt-[25px]">
