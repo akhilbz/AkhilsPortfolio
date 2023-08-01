36603d1c3a471a2c8706e134655f5ce9928c7aa9
akhilbz <akhilesh.bitla@gmail.com>
2023-07-31 23:45:45 -0500

    removed carousel and left one portfolio pic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,34 +6,34 @@ import portPic4 from "../assets/portpics/port-pic4.jpg";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled, RxDot } from "react-icons/rx";
 const About = () => {
-    const [currIndex, setCurrIndex] = useState(0);
-    const gallery = [
-        {
-            url: portPic1
-        },
-        {
-            url: portPic2
-        },
-        {
-            url: portPic3
-        },
-        {
-            url: portPic4
-        },
-    ];
-    const prevSlide = () => {
-        const isFirstSlide = currIndex === 0;
-        const newIndex = isFirstSlide ? gallery.length - 1 : currIndex - 1;
-        setCurrIndex(newIndex);
-    }
-    const nextSlide = () => {
-        const isLastSlide = currIndex === gallery.length - 1;
-        const newIndex = isLastSlide ? 0 : currIndex + 1;
-        setCurrIndex(newIndex);
-    }
-    const goToImage = (imageIndex) => {
-        setCurrIndex(imageIndex);
-    }
+    // const [currIndex, setCurrIndex] = useState(0);
+    // const gallery = [
+    //     {
+    //         url: portPic1
+    //     },
+    //     {
+    //         url: portPic2
+    //     },
+    //     {
+    //         url: portPic3
+    //     },
+    //     {
+    //         url: portPic4
+    //     },
+    // ];
+    // const prevSlide = () => {
+    //     const isFirstSlide = currIndex === 0;
+    //     const newIndex = isFirstSlide ? gallery.length - 1 : currIndex - 1;
+    //     setCurrIndex(newIndex);
+    // }
+    // const nextSlide = () => {
+    //     const isLastSlide = currIndex === gallery.length - 1;
+    //     const newIndex = isLastSlide ? 0 : currIndex + 1;
+    //     setCurrIndex(newIndex);
+    // }
+    // const goToImage = (imageIndex) => {
+    //     setCurrIndex(imageIndex);
+    // }
     return (
         <div name="about" className="w-full h-[fit-content] bg-[#333333]">
             <div className="flex flex-col justify-center items-center w-full h-[fit-content]">
@@ -52,26 +52,26 @@ const About = () => {
                         </div>
                         </div>
                     </div>
-                    <div className="xl:pt-[45px] pt-8">
+    
                     <div className="justify-center items-center flex flex-col sm:mx-[170px] relative rounded-2xl bg-center bg-cover h-[645px] ">
-                    <div style={{backgroundImage: `url(${gallery[currIndex].url})`, boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.75)' }} className=" group w-[390px] h-[590px] items-center flex justify-between rounded-2xl bg-center bg-cover duration-500">
-                    <div className="hidden group-hover:block justify-start text-3xl p-2 text-white cursor-pointer">
+                    <div style={{backgroundImage: `url(${portPic1})`, boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.75)' }} className=" group w-[390px] h-[590px] items-center flex justify-between rounded-2xl bg-center bg-cover duration-500">
+                    {/* <div className="hidden group-hover:block justify-start text-3xl p-2 text-white cursor-pointer">
                         <BsChevronCompactLeft onClick={prevSlide} size={35} />
                     </div>
                     <div className=""></div>
                     <div className="hidden group-hover:block justify-end text-3xl p-2 text-white cursor-pointer">
                         <BsChevronCompactRight onClick={nextSlide} size={35}/>
-                    </div>
+                    </div> */}
                     </div> 
-                    <div className="flex flex-row justify-center items-center md:pb-8 mt-2 px-[147px] ">
+                    {/* <div className="flex flex-row justify-center items-center md:pb-8 mt-2 px-[147px] ">
                         {gallery.map((image, imageIndex) => (
                             <div key={imageIndex} onClick={() => goToImage(imageIndex)} className="text-2xl cursor-pointer">
                                {imageIndex == currIndex ? <RxDotFilled style={{color: "white"}}/> : <RxDot style={{color: "white"}}/>}
                             </div>
                         ))}
-                    </div>  
+                    </div>   */}
                     </div> 
-                    </div>  
+                    {/* </div>   */}
                 </div>
             </div>
         </div>
