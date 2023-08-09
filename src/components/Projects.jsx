468ceaa43b323cf7cfc758752a0a619d1fdf669e
akhilbz <akhilesh.bitla@gmail.com>
2023-08-08 19:16:27 -0500

    Updated Project section and need to make some more minor changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import Trove from '../assets/trove.png';
 import Organize from '../assets/organize-logo.png';
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub, FaYoutube,  } from 'react-icons/fa';
 import Devpost from '../assets/devpost.svg';
+import JS from '../assets/stack-icons/javascript.png';
+import HTML from '../assets/stack-icons/html.png';
+import CSS from '../assets/stack-icons/css.png';
+import React_logo from '../assets/stack-icons/react.png';
+import Redux from '../assets/stack-icons/redux.svg';
+import Bootstrap from '../assets/stack-icons/bootstrap.png';
+import Webpack from '../assets/stack-icons/webpack.png';
+import Java from '../assets/stack-icons/java.png';
+import MongoDB from '../assets/stack-icons/mongo.png';
+import SpringBoot from '../assets/stack-icons/springboot.png';
+import Figma from '../assets/stack-icons/figma.svg';
+
 const Projects = () => {
   return (
     <div name="projects" className='w-full h-full bg-[#1A1A1A] pt-[75px]'>
@@ -16,12 +28,27 @@ const Projects = () => {
           <div className="lg:flex-row flex-col flex lg:justify-between justify-center items-center lg:mr-8 lg:ml-[80px]">
             
             <div className="border-b-2 border-t-2 my-6 py-4 border-[#F7760E] xl:w-[750px] lg:w-[500px] hidden lg:flex flex-col text-white justify-center font-medium xl:text-xl text-lg ml-5 ">
-              <p className='pb-5'>&#8226; Developed a Chrome extension designed to enhance your tab management experience and optimize
+            <div className="flex-row flex px-2">
+            <p>Tech Stack:</p>
+            <img src={React_logo} alt="" className=' w-7 mx-2 hover:scale-95 duration-500' />
+            <img src={JS} alt="" className=' w-7 mx-2 hover:scale-95 duration-500' />
+            <img src={HTML} alt="" className=' w-7 mx-2 hover:scale-95 duration-500' />
+            <img src={CSS} alt="" className=' w-7 mx-2 hover:scale-95 duration-500' />
+            <img src={Redux} alt="" className=' w-7 mx-2 hover:scale-95 duration-500' />
+            <img src={Bootstrap} alt="" className=' w-7 mx-2 hover:scale-95 duration-500' />
+            <img src={Webpack} alt="" className=' w-7 mx-2 hover:scale-95 duration-500' />
+            </div>
+              <p className='py-3 px-2'><span className='font-bold text-2xl text-[#c2adff]'>Organize</span> is a chrome extension designed to enhance your tab management experience and optimize
                             productivity and organization. Implemented intuitive features for effortless tab organization, including
                             quick grouping, fast searching, and more.</p>
-              <p className='pb-5'>&#8226; Self-taught React, HTML, and CSS to develop both front-end and back-end skills. Utilized Chrome
-                 Developer APIs ‘tabs’ and ‘tabGroups’ to manage tabs based on state.</p>
-              <p>&#8226; Tech Stack: JavaScript, React, Redux, HTML, CSS3, BootStrap 5, Webpack</p>
+              <div className="flex-row flex w-full justify-center">
+              {/* <button className='flex mx-3'>
+              <span className='border-2 border-[#F7760E] font-semibold rounded hover:bg-[#F7760E] duration-500 text-[#F7760E] xl:px-2 px-6 hover:text-[#121212]'>&nbsp;Send&nbsp;</span>
+              </button> */}
+              <button className='flex mx-3'>
+              <span className='border-2 border-[#F7760E]  h-[50px] font-semibold rounded hover:bg-[#F7760E] duration-500 text-[#F7760E] xl:px-2 flex items-center text-2xl hover:text-[#121212]'>&nbsp;<FaYoutube />&nbsp;Demo&nbsp;</span>
+              </button>
+              </div>
             </div>
             <div className="sm:w-[400px] w-[350px] sm:h-[400px] h-[350px] bg-[#121212] flex-col z-10 flex justify-center lg:py-9 items-center rounded-xl hover:scale-105 duration-500 overflow-hidden">
               {/* <p id='tap-read' className='text-white h-0 lg:pb-9 pb-[70px] font-light text-sm flex lg:hidden'>Tap to read</p>*/}
@@ -31,26 +58,41 @@ const Projects = () => {
                 </div>
               </div> */}
               <img src={Organize} alt="" className='h-[250px] w-[250px] rounded-2xl'/>
-              <p className='text-white text-2xl'>Organize Chrome Extension</p>
+              <p className='text-white text-2xl flex'>Organize Chrome Extension</p>
             </div>
             <div className="border-b-2 border-t-2 sm:mx-[120px] mx-[60px] my-6 py-4 border-[#F7760E] text-white lg:hidden flex flex-col justify-center font-medium sm:text-lg text-base">
-              <p className='pb-5'>&#8226; Developed a Chrome extension designed to enhance your tab management experience and optimize
-                            productivity and organization. Implemented intuitive features for effortless tab organization, including
-                            quick grouping, fast searching, and more.</p>
-              <p className='pb-5'>&#8226; Self-taught React, HTML, and CSS to develop both front-end and back-end skills. Utilized Chrome
-                 Developer APIs ‘tabs’ and ‘tabGroups’ to manage tabs based on state.</p>
-              <p>&#8226; Tech Stack: JavaScript, React, Redux, HTML, CSS3, BootStrap 5, Webpack</p>
+              <div className="flex-row flex justify-center w-full ">
+              <p>Tech Stack:</p>
+              <img src={React_logo} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={JS} alt="" className=' w-8 h-8  mx-2 hover:scale-95 duration-500' />
+              <img src={HTML} alt="" className=' w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={CSS} alt="" className=' w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={Redux} alt="" className=' w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={Bootstrap} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={Webpack} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              </div>
+                <p className='py-3 px-2'><span className='font-bold text-xl text-[#c2adff]'>Organize</span> is a chrome extension designed to enhance your tab management experience and optimize
+                              productivity and organization. Implemented intuitive features for effortless tab organization, including
+                              quick grouping, fast searching, and more.</p>
+                <div className="flex-row flex w-full justify-center">
+                {/* <button className='flex mx-3'>
+                <span className='border-2 border-[#F7760E] font-semibold rounded hover:bg-[#F7760E] duration-500 text-[#F7760E] xl:px-2 px-6 hover:text-[#121212]'>&nbsp;Send&nbsp;</span>
+                </button> */}
+                <button className='flex mx-3'>
+                <span className='border-2 border-[#F7760E] sm:h-[50px] font-semibold rounded hover:bg-[#F7760E] duration-500 text-[#F7760E] xl:px-2  flex items-center text-xl hover:text-[#121212]'>&nbsp;<FaYoutube />&nbsp;Demo&nbsp;</span>
+                </button>
+                </div>
             </div>
           </div>
 
           <div className="lg:flex-row flex-col flex lg:justify-between justify-center items-center lg:mr-8 lg:ml-[80px] mt-7 sm:mt-[80px]">
             <div className="sm:w-[400px] w-[350px] sm:h-[400px] h-[350px]  bg-[#121212] flex-col flex justify-center lg:py-9 items-center rounded-xl hover:scale-105 duration-500 overflow-hidden">
               {/* <p className='text-white h-0 lg:pb-9 pb-[70px] font-light text-sm flex lg:hidden'>Tap to read</p>         */}
-              <div className="w-full flex flex-col justify-start mt-5">
+              {/* <div className="w-full flex flex-col justify-start mt-5">
                 <div className=" bg-white hover:bg-[#0087c1] hover:text-white duration-500 h-9 flex flex-col rounded-br-xl rounded-tr-xl justify-center w-[45px] z-50 ">
                   <a href="https://devpost.com/software/found-b1p3x7" target="_blank"><img src={Devpost} alt="" className='h-[32px] w-[32px] ml-[7px]'/></a>
                 </div>
-              </div>
+              </div> */}
               {/* <div className="w-full">
                 <div className="flex justify-start flex-row">
                   <div className="bg-white hover:bg-[#0087c1] duration-500 h-9 flex rounded-br-xl rounded-tr-xl mt-5 justify-center flex-col w-[45px] z-50 "> */}
@@ -62,18 +104,51 @@ const Projects = () => {
               <p className='text-white text-2xl p-2'>Trove: The Lost & Found App</p>
             </div>
             <div className="border-b-2 border-t-2 sm:mx-[120px] mx-[60px] my-6 py-4 border-[#F7760E] text-white lg:hidden flex flex-col justify-center font-medium sm:text-lg text-base">
-              <p className='pb-5'>&#8226; Developed a virtual service platform that allows college students to report lost items and claim lost
-              valuables through a student network.</p>
-              <div className='pb-5'>&#8226; Built within 24 hours with three peers and won Best Overall on Beginner’s Track Award out of 75 teams in
-              HackTX 2022 (Freetail Hackers Hackathon)</div>
-              <p>&#8226; Tech Stack: Java, JavaScript, Spring Framework, React Native, MongoDB, Figma</p>
+            <div className="flex-row flex  w-full ">
+              <p>Tech Stack:</p>
+              <img src={React_logo} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={JS} alt="" className=' w-8 h-8  mx-2 hover:scale-95 duration-500' />
+              <img src={HTML} alt="" className=' w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={CSS} alt="" className=' w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={Java} alt="" className=' w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={MongoDB} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={Figma} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              </div>
+              <p className='py-3'><span className='font-bold text-xl text-[#c2adff]'>Trove</span> is a virtual service platform that allows college students to report lost items and claim lost
+              valuables through a student network. Trove was built as a hackathon project with three peers and a time constraint of 24 hours. Our team won the <span className='text-[#FFD700] text-xl font-bold'>Best Overall Beginner Hack Award</span> out of  <span className='text-[#FFD700] text-xl font-bold'>75 teams</span> in
+              HackTX 2022 (Freetail Hackers Hackathon).</p>
+              <div className="flex-row flex w-full justify-center">
+              <button className='flex mx-3'>
+                <a href="https://devpost.com/software/found-b1p3x7" target="_blank"><span className='border-2 border-[#F7760E] hover:border-white sm:h-[50px] font-semibold rounded bg-[#F7760E] hover:bg-white duration-500 xl:px-2  flex items-center text-xl text-[#121212]'>&nbsp;<img src={Devpost} alt="" className='h-[25px] w-[25px]'/>&nbsp;Devpost&nbsp;</span></a>
+                </button>
+                <button className='flex mx-3'>
+                  <a href="https://www.youtube.com/watch?v=Zl_hXwML1_o" target="_blank"><span className='border-2 border-[#F7760E] sm:h-[50px] font-semibold rounded hover:bg-[#F7760E] duration-500 text-[#F7760E] xl:px-2  flex items-center text-xl hover:text-[#121212]'>&nbsp;<FaYoutube />&nbsp;Demo&nbsp;</span></a>
+                </button>
+                </div>
             </div>
+
             <div className="border-b-2 border-t-2 my-6 py-4 border-[#F7760E] text-white xl:w-[750px] lg:w-[500px] hidden lg:flex flex-col justify-center font-medium xl:text-xl text-lg mr-5">
-              <p className='pb-5'>&#8226; Developed a virtual service platform that allows college students to report lost items and claim lost
-              valuables through a student network.</p>
-              <div className='pb-5'>&#8226; Built within 24 hours with three peers and won Best Overall on Beginner’s Track Award out of 75 teams in
-              HackTX 2022 (Freetail Hackers Hackathon)</div>
-              <p>&#8226; Tech Stack: Java, JavaScript, Spring Framework, React Native, MongoDB, Figma</p>
+              <div className="flex-row flex  w-full ">
+              <p>Tech Stack:</p>
+              <img src={React_logo} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={JS} alt="" className=' w-8 h-8  mx-2 hover:scale-95 duration-500' />
+              <img src={HTML} alt="" className=' w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={CSS} alt="" className=' w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={Java} alt="" className=' w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={MongoDB} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              <img src={Figma} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
+              </div>
+              <p className='py-3'><span className='font-bold text-2xl text-[#c2adff]'>Trove</span> is a virtual service platform that allows college students to report lost items and claim lost
+              valuables through a student network. Trove was built as a hackathon project with three peers and a time constraint of 24 hours. Our team won the <span className='text-[#FFD700] text-2xl font-bold'>Best Overall Beginner Hack Award</span> out of  <span className='text-[#FFD700] text-2xl font-bold'>75 teams</span> in
+              HackTX 2022 (Freetail Hackers Hackathon).</p>
+              <div className="flex-row flex w-full justify-center">
+              <button className='flex mx-3'>
+                <a href="https://devpost.com/software/found-b1p3x7" target="_blank"><span className='border-2 border-[#F7760E] hover:border-white sm:h-[50px] font-semibold rounded bg-[#F7760E] hover:bg-white duration-500 xl:px-2  flex items-center text-2xl text-[#121212]'>&nbsp;<img src={Devpost} alt="" className='h-[32px] w-[32px]'/>&nbsp;Devpost&nbsp;</span></a>
+                </button>
+                <button className='flex mx-3' >
+                <a href="https://www.youtube.com/watch?v=Zl_hXwML1_o" target="_blank"><span className='border-2 border-[#F7760E] sm:h-[50px] font-semibold rounded hover:bg-[#F7760E] duration-500 text-[#F7760E] xl:px-2  flex items-center text-2xl hover:text-[#121212]'>&nbsp;<FaYoutube />&nbsp;Demo&nbsp;</span></a>
+                </button>
+                </div>
             </div>
           </div>
         </div>
