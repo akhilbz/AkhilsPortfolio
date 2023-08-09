82fddc39c82bc604c2e0ac0a2944313125b17eba
akhilbz <akhilesh.bitla@gmail.com>
2023-08-09 12:38:00 -0500

    Completed projects section; only video recordings are remaining

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -42,22 +42,14 @@ const Projects = () => {
               <p className='py-3 px-2'><span className='font-bold text-2xl text-[#8dd9f7]'>Organize</span> is a chrome extension designed to enhance your tab management experience and optimize
                             productivity and organization. Implemented intuitive features for effortless tab organization, including
                             quick grouping, fast searching, and more.</p>
+              <p className='py-3 px-2'>This project is my first standalone front-end project that uses chrome developer APIs to manage and update the state of tabs and its properties through the extension.</p>
               <div className="flex-row flex w-full justify-center">
-              {/* <button className='flex mx-3'>
-              <span className='border-2 border-[#F7760E] font-semibold rounded hover:bg-[#F7760E] duration-500 text-[#F7760E] xl:px-2 px-6 hover:text-[#121212]'>&nbsp;Send&nbsp;</span>
-              </button> */}
               <button className='flex mx-3'>
               <span className='border-2 border-[#F7760E]  h-[50px] font-semibold rounded hover:bg-[#F7760E] duration-500 text-[#F7760E] xl:px-2 flex items-center text-2xl hover:text-[#121212]'>&nbsp;<FaYoutube />&nbsp;Demo&nbsp;</span>
               </button>
               </div>
             </div>
             <div className="sm:w-[400px] w-[350px] sm:h-[400px] h-[350px] bg-[#121212] flex-col z-10 flex justify-center lg:py-9 items-center rounded-xl hover:scale-105 duration-500 overflow-hidden">
-              {/* <p id='tap-read' className='text-white h-0 lg:pb-9 pb-[70px] font-light text-sm flex lg:hidden'>Tap to read</p>*/}
-              {/* <div className="w-full flex justify-end ml-[90px] overflow-hidden">
-                <div className=" bg-white hover:bg-black hover:text-white duration-500 h-9 flex flex-col rounded-bl-xl rounded-tl-xl justify-center w-[90px] z-50 ">
-                  <a href="https://github.com/akhilbz/Organize" target='_blank'><FaGithub size={30} className='ml-2'/></a>
-                </div>
-              </div> */}
               <img src={Organize} alt="" className='h-[250px] w-[250px] rounded-2xl'/>
               <p className='text-white text-2xl flex'>Organize Chrome Extension</p>
             </div>
@@ -75,10 +67,8 @@ const Projects = () => {
                 <p className='py-3 px-2'><span className='font-bold text-xl text-[#8dd9f7]'>Organize</span> is a chrome extension designed to enhance your tab management experience and optimize
                               productivity and organization. Implemented intuitive features for effortless tab organization, including
                               quick grouping, fast searching, and more.</p>
+                <p className='py-3 px-2'>This project is my first standalone front-end project that uses chrome developer APIs to manage and update the state of tabs and its properties through the extension.</p>
                 <div className="flex-row flex w-full justify-center">
-                {/* <button className='flex mx-3'>
-                <span className='border-2 border-[#F7760E] font-semibold rounded hover:bg-[#F7760E] duration-500 text-[#F7760E] xl:px-2 px-6 hover:text-[#121212]'>&nbsp;Send&nbsp;</span>
-                </button> */}
                 <button className='flex mx-3'>
                 <span className='border-2 border-[#F7760E] sm:h-[50px] font-semibold rounded hover:bg-[#F7760E] duration-500 text-[#F7760E] xl:px-2  flex items-center text-xl hover:text-[#121212]'>&nbsp;<FaYoutube />&nbsp;Demo&nbsp;</span>
                 </button>
@@ -88,24 +78,11 @@ const Projects = () => {
 
           <div className="lg:flex-row flex-col flex lg:justify-between justify-center items-center lg:mr-8 lg:ml-[80px] mt-7 sm:mt-[80px]">
             <div className="sm:w-[400px] w-[350px] sm:h-[400px] h-[350px]  bg-[#121212] flex-col flex justify-center lg:py-9 items-center rounded-xl hover:scale-105 duration-500 overflow-hidden">
-              {/* <p className='text-white h-0 lg:pb-9 pb-[70px] font-light text-sm flex lg:hidden'>Tap to read</p>         */}
-              {/* <div className="w-full flex flex-col justify-start mt-5">
-                <div className=" bg-white hover:bg-[#0087c1] hover:text-white duration-500 h-9 flex flex-col rounded-br-xl rounded-tr-xl justify-center w-[45px] z-50 ">
-                  <a href="https://devpost.com/software/found-b1p3x7" target="_blank"><img src={Devpost} alt="" className='h-[32px] w-[32px] ml-[7px]'/></a>
-                </div>
-              </div> */}
-              {/* <div className="w-full">
-                <div className="flex justify-start flex-row">
-                  <div className="bg-white hover:bg-[#0087c1] duration-500 h-9 flex rounded-br-xl rounded-tr-xl mt-5 justify-center flex-col w-[45px] z-50 "> */}
-                    {/* <a href="https://devpost.com/software/found-b1p3x7" target="_blank"><img src={Devpost} alt="" className='h-[32px] w-[32px] ml-[7px]'/></a> */}
-                  {/* </div> */}
                   <img src={Trove} alt="" className='h-[250px] w-[250px] rounded-2xl'/>
-                {/* </div> */}
-              {/* </div> */}
               <p className='text-white text-2xl p-2'>Trove: The Lost & Found App</p>
             </div>
             <div className="border-b-2 border-t-2 sm:mx-[120px] mx-[60px] my-6 py-4 border-[#F7760E] text-white lg:hidden flex flex-col justify-center font-medium sm:text-lg text-base">
-            <div className="flex-row flex w-full ">
+            <div className="flex-row flex justify-center items-center w-full ">
               <p>Tech Stack:</p>
               <img src={React_logo} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
               <img src={JS} alt="" className=' w-8 h-8  mx-2 hover:scale-95 duration-500' />
@@ -130,7 +107,7 @@ const Projects = () => {
             </div>
 
             <div className="border-b-2 border-t-2 my-6 py-4 border-[#F7760E] text-white xl:w-[750px] lg:w-[500px] hidden lg:flex flex-col justify-center font-medium xl:text-xl text-lg mr-5">
-              <div className="flex-row flex w-full ">
+              <div className="flex-row  flex w-full ">
               <p>Tech Stack:</p>
               <img src={React_logo} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
               <img src={JS} alt="" className=' w-8 h-8  mx-2 hover:scale-95 duration-500' />
@@ -158,7 +135,7 @@ const Projects = () => {
           <div className="lg:flex-row flex-col flex lg:justify-between justify-center items-center lg:mr-8 lg:ml-[80px]  mt-7 sm:mt-[80px]">
             
             <div className="border-b-2 border-t-2 my-6 py-4 border-[#F7760E] xl:w-[750px] lg:w-[500px] hidden lg:flex flex-col text-white justify-center font-medium xl:text-xl text-lg ml-5 ">
-            <div className="flex-row flex px-2 justify-center items-center w-full">
+            <div className="flex-row flex px-2 justify-start items-center w-full">
               <p>Tech Stack:</p>
               <img src={React_logo} alt="" className='w-8 h-8 mx-2 hover:scale-95 duration-500' />
               <img src={JS} alt="" className=' w-8 h-8  mx-2 hover:scale-95 duration-500' />
@@ -184,7 +161,7 @@ const Projects = () => {
               </div>
             </div>
             <div className="sm:w-[400px] w-[350px] sm:h-[400px] h-[350px] bg-[#121212] flex-col z-10 flex justify-center lg:py-9 items-center rounded-xl hover:scale-105 duration-500 overflow-hidden">
-              <img src={MovieDB} alt="" className=' inset-0 w-[375px] h-full rounded-2xl'/>
+              <img src={MovieDB} alt="" className=' max-w-[385px] w-[100%] h-full rounded-2xl' style={{aspectRatio: '3022/1614'}}/>
               <p className='text-white text-2xl flex'>MovieDB Clone</p>
             </div>
             <div className="border-b-2 border-t-2 sm:mx-[120px] mx-[60px] my-6 py-4 border-[#F7760E] text-white lg:hidden flex flex-col justify-center font-medium sm:text-lg text-base">
